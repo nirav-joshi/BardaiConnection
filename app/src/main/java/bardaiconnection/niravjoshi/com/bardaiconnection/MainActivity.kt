9e1e2833d22ac2bb6715b3ce222ced791f321876
Nirav Joshi <niravjoshi.gdcst@gmail.com>
2019-05-12 20:25:36 +0530

    Initial Commit

---
+++ app/src/main/java/bardaiconnection/niravjoshi/com/bardaiconnection/MainActivity.kt
@@ -0,0 +1,12 @@
+package bardaiconnection.niravjoshi.com.bardaiconnection
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
